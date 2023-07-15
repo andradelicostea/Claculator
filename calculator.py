6c6ce0b435c38cdc651d5af986dace41d349d268
Andra-Simona Delicostea <126616500+andradelicostea@users.noreply.github.com>
2023-07-15 11:58:10 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 import re
 
+
 print("Our calculator")
 print("Type 'quit' to exit.\n")
 
@@ -10,20 +11,26 @@ def performMath():
     global run
     global previous
     equation=""
+
     if previous==0:
         equation=input("Enter equation: ")
     else:
         equation=input(str(previous))
+
+
     if equation=='quit':
         print("Goodbye!")
         run=False
     else:
         equation=re.sub('[a-zA-Z,.:" "]', '', equation)
+
         if previous==0:
             previous=eval(equation)
         else:
             previous=eval(str(previous)+equation)
+
         print("You typed:", previous)
+
 
 while run:
     performMath()
